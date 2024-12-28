35151656657e1fe345a70a7810b83d6ce9dc2baf
Ivan Shakhman <shakhmanivan@gmail.com>
2024-12-28 16:37:23 +0200

    create base trade manager with logging

---
+++ trades/trade_manager.py
@@ -0,0 +1,25 @@
+from loguru import logger
+import time
+import random
+
+
+class TradeManager:
+
+    def __init__(self, account, proxy, user_agent, web3):
+        self.account = account
+        self.proxy = proxy
+        self.user_agent = user_agent
+        self.web3 = web3
+
+    def open_position(self, amount, position_type):
+        logger.info(f"Opening {position_type} position for {self.account} with {amount}")
+
+    def close_position(self):
+        logger.info(f"Closing position for {self.account}")
+
+    def trade(self, num_transactions, delay_range=(0, 10)):
+        for _ in range(num_transactions):
+            amount = random.uniform(1, 100)
+            self.open_position(amount)
+            time.sleep(random.uniform(*delay_range))
+            self.close_position()
